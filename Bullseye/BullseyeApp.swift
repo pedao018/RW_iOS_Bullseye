4dcd991b7c33fa06982b041d5fce7ba28d376a40
KeiKa <dhainguyen018@gmail.com>
2021-07-05 01:07:09 +0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Vo Thi Hong Diem on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
